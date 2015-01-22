fbf3da94ccb3bdba5f0910f05dfae0ffe213633e
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2015-01-22 13:53:20 +0100

    version 1.0.4

--- audio/about_psychoacoustics.py
+++ audio/about_psychoacoustics.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__        = "audio.psychoacoustics",
-  __version__     = "1.0.3",
+  __version__     = "1.0.4",
   __license__     = "MIT License",  
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__         = None,
